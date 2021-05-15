b94b94259f395bd496ed5f3625219b70ca7d7304
gamer <gamer@DESKTOP-91GD9VI.fritz.box>
2021-05-15 14:32:35 +0200

    Test 187

--- Seschat Buchhaltung/src/test/Deeznuts.java
+++ Seschat Buchhaltung/src/test/Deeznuts.java
@@ -3,6 +3,6 @@ package test;
 public class Deeznuts {
 		public static void main(String[] args) {
 			System.out.println("Annegret Weng");
-			System.out.println("");
+			System.out.println("yeet");
 		}
 }
