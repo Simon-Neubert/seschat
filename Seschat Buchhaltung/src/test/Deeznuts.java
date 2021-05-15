33e5baef89c7f5225ee905b00bf1e38e90957252
Timmy Töpfer <02toti1bwi@hft-stuttgart.de>
2021-05-15 14:42:20 +0200

    abc

--- Seschat Buchhaltung/src/test/Deeznuts.java
+++ Seschat Buchhaltung/src/test/Deeznuts.java
@@ -4,6 +4,6 @@ public class Deeznuts {
 		public static void main(String[] args) {
 			System.out.println("Annegret Weng");
 			System.out.println("yeet");
-			System.out.println("Mosler Abi");
+			System.out.println("Mosler Abii");
 		}
 }
